64503bd551b2466dc779b9d5bdff12455ac6a44f
yuanmeng <372909885@qq.com>
2018-04-07 12:06:14 +0800

    修改login

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -258,7 +258,7 @@ module.exports = {
                       loader: require.resolve('less-loader'),
                       options: {
                         modifyVars:{
-                          "@primary-color":"#1DA57A"
+                          // "@primary-color":"#1DA57A"
                         },
                         javascriptEnabled: true
                       }
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,64 @@
 import React from 'react'
+import './index.less'
+import {Form,Input,Button} from 'antd'
+const FormItem  = Form.Item
 
 export default class Login extends React.Component{
     render (){
         return (
-            <div>login</div>
+            <div className='login-page'>
+                <div className='login-content-wrap'>
+                    <div className='login-content'>
+                        <div className='word'>珠峰培训<br/>ERP管理系统</div>
+                        <div className='login-box'>
+                            <div className='title'>欢迎您</div>
+                            <LoginForm/>
+                         </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
+
+class LoginForm extends React.Component{
+    checkUserName=(rule,value,callback)=>{
+        let reg = /^1\d{10}$/;
+        if(!value){
+            callback('请输入用户名')
+        }else if (!reg.test(value)){
+            callback('用户名错误')
+        }else {
+            callback()
+        }
+    }
+    render (){
+        const {getFieldDecorator} =this.props.form
+        return (
+            <Form>
+                <FormItem>
+                    {
+                       getFieldDecorator('username',{
+                            rules:[{validator:this.checkUserName}]
+                       })(
+                            <Input placeholder='用户名'/>
+                       ) 
+                    }
+                </FormItem>
+                <FormItem>
+                    {
+                        getFieldDecorator('password',{
+                            rules:[{required:true,message:'请输入密码'}]
+                        })(
+                            <Input placeholder='密码' type='password'/>
+                        )
+                    }
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" className='login-form-button'>登录</Button>
+                </FormItem>
+            </Form>    
+        )
+    }
+}
+LoginForm = Form.create({})(LoginForm)
