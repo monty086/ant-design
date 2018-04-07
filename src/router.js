1627d049fa22a800dc365db0900d1c6ecaf937ad
yuanmeng <372909885@qq.com>
2018-04-07 11:00:02 +0800

    提交修改

---
+++ src/router.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Link,Switch,Route,Router,Redirect,} from 'react-router-dom'
+import history from './history'
+import App from './App'
+import Login from './pages/login'
+import Home from './pages/home'
+import Admin from './pages/admin'
+import Order from './pages/order'
+
+export default class ERoute extends React.Component{
+    render(){
+        return (
+            <Router history={history}>
+                <Switch>
+                    <Route path='/'>
+                        <App>
+                            <Switch>
+                                <Route exact path='/' component={Login}/>
+                                <Route path='/home'>
+                                    <Home>
+                                        <Switch>
+                                            <Route exact path='/home' component={Admin}/>
+                                            <Route  path='/home/order' component={Order}/>
+                                            <Redirect to='/home'/>
+                                        </Switch>
+                                    </Home>
+                                </Route>
+                                <Redirect to='/' />
+                            </Switch>
+                        </App>
+                    </Route>
+                </Switch>
+            </Router>  
+        )
+    }
+}
